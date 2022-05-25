37a2e6ba07f7a7790836975506ac243dcc7bb946
Azie Melza Pratama <aziemp66@gmail.com>
2022-05-25 13:27:51 +0700

    Project Init

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
