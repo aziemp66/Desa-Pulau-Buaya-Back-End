dc22c635c90753385625151664abf84b03ca0e83
Azie Melza Pratama <aziemp66@gmail.com>
2022-08-09 20:03:49 +0700

    Adding Forgot and Reset Password

--- Controllers/Auth.controller.js
+++ Controllers/Auth.controller.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const emailSender = require("../Utility/NodeMailer");
 
 const User = require("../Models/User.model");
 
@@ -75,7 +76,81 @@ const loginUser = async (req, res, next) => {
   res.json({ token, message: "Successfully Logged in" });
 };
 
+const forgotPassword = async (req, res, next) => {
+  const { email } = req.body;
+
+  let user;
+  try {
+    user = await User.findOne({ email });
+  } catch (err) {
+    return next(err);
+  }
+  if (!user) return next(new Error("User not found"));
+
+  const resetPasswordToken = resetPassword.generateLink(
+    { email: user.email, password: user.password },
+    `${process.env.JWT_SECRET}${user.password}`
+  );
+  const resetPasswordLink = `${process.env.FRONTEND_URL}/reset-password?id=${user._id}&token=${resetPasswordToken}`;
+
+  try {
+    emailSender({
+      to: user.email,
+      subject: "Reset Password",
+      html: `<p>Click <a href="${resetPasswordLink}">here</a> to reset your password</p>`,
+      text: `Click here to reset your password: ${resetPasswordLink}`,
+    });
+  } catch (error) {
+    return next(error);
+  }
+
+  res.json({ message: "Reset Password Link has been sent to your email" });
+};
+
+const resetUserPassword = async (req, res, next) => {
+  const { password, confirmPassword } = req.body;
+  const { id, token } = req.query;
+
+  const { error } = validation.passwordChangeValidation({
+    password,
+    confirmPassword,
+  });
+  if (error) return next(error.details[0]);
+
+  let user;
+  try {
+    user = await User.findOne({ id: id });
+  } catch (err) {
+    return next(err);
+  }
+  if (!user) return next(new Error("User not found"));
+
+  let isVerified;
+  try {
+    isVerified = resetPassword.verifyLink(
+      token,
+      `${process.env.JWT_SECRET}${user.password}`
+    );
+  } catch (err) {
+    return next(err);
+  }
+  if (!isVerified) return next(new Error("Link is invalid"));
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  user.password = hashedPassword;
+
+  try {
+    await user.save();
+    res.json({ message: "Password changed successfully" });
+  } catch (err) {
+    return next(err);
+  }
+};
+
 module.exports = {
   register: registerUser,
   login: loginUser,
+  forgotPassword: forgotPassword,
+  resetPassword: resetUserPassword,
 };
--- Router/Auth.routes.js
+++ Router/Auth.routes.js
@@ -6,4 +6,8 @@ router.post("/login", authController.login);
 
 router.post("/register", authController.register);
 
+router.post("/forgot-password", authController.forgotPassword);
+
+router.post("/reset-password", authController.resetPassword);
+
 module.exports = router;
